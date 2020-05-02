348b98f77ebf112565e4f48e3873de429824bda7
Dmytro Boichenko <dmytro.boichenko.dev@gmail.com>
2020-05-02 14:48:08 +0200

    Initial commit

---
+++ src/main/java/io/vertx/guides/wiki/MainVerticle.java
@@ -0,0 +1,31 @@
+package io.vertx.guides.wiki;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Promise;
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start(Promise<Void> promise) {
+        Promise<String> dbVerticleDeployment = Promise.promise();
+        vertx.deployVerticle(new WikiDatabaseVerticle(), dbVerticleDeployment);
+
+        dbVerticleDeployment.future()
+            .compose(id -> {
+                Promise<String> httpVerticleDeployment = Promise.promise();
+                vertx.deployVerticle("io.vertx.guides.wiki.HttpServerVerticle",
+                    new DeploymentOptions().setInstances(2),
+                    httpVerticleDeployment);
+
+                return httpVerticleDeployment.future();
+            })
+            .setHandler(ar -> {
+                if (ar.succeeded()) {
+                    promise.complete();
+                } else {
+                    promise.fail(ar.cause());
+                }
+            });
+    }
+}
